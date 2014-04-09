310aae4e5695da233094ef47ee8ab7b6ea10ecdb
lauraprem <premillieu.laura@gmail.com>
2014-04-09 09:00:14 +0200

    project

---
+++ Gomoku/src/Joueurs/Joueur.java
@@ -0,0 +1,19 @@
+
+package Joueurs;
+
+import java.util.Random;
+/**
+ *
+ * @author Laura Prémillieu && Corinne Fagno
+ */
+public class Joueur 
+{
+protected int id;
+
+public Joueur(int _id)
+{
+    this.id=_id;
+}
+
+
+}
